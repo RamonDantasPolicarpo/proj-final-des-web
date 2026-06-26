b93f5ea4c79223308397a3333357dc9656b81917
Ramon Dantas Policarpo <ramonpolicarpo36@gmail.com>
2026-06-26 00:50:17 -0300

    Tentando descobrir pq a variável de ambiente não funciona

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+console.log("Variável carregada durante o build:", process.env.VUE_APP_API_URL);
+
 createApp(App).use(router).mount('#app')
